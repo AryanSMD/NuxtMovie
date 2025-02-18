20a6d272d3c775237449c2494e8773485496f7d2
AryanSMD <aryansmd79@gmail.com>
2025-02-18 17:44:38 +0330

    updated

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -21,9 +21,9 @@ const title = computed(() => {
     >
         <div class="title" v-html="title"></div>
         <div class="description">{{ t('description') }}</div>
-        <div class="w-full flex items-center justify-center gap-4">
-            <nuxt-link click="btn" to="/movies">Movies</nuxt-link>
-            <nuxt-link click="btn" to="/series">Series</nuxt-link>
+        <div class="w-full flex items-center justify-center gap-[80px] my-[40px]">
+            <nuxt-link class="btn" to="/movies">{{ t('movies') }}</nuxt-link>
+            <nuxt-link class="btn" to="/series">{{ t('series') }}</nuxt-link>
         </div>
     </div>
 </template>
@@ -37,5 +37,34 @@ const title = computed(() => {
 .description {
     @apply
     text-justify w-fit sm:w-[500px] lg:w-[700px]
+}
+.btn {
+    box-shadow: 
+        1px 1px 5px var(--primary),
+        .5px .5px 10px var(--primary),
+        -1px -1px 5px var(--primary),
+        -.5px -.5px 10px var(--primary),
+        2.5px 2.5px 10px var(--primary) inset,
+        -2.5px -2.5px 10px var(--primary) inset;
+    text-shadow: 
+        0 0 15px var(--primary);
+    @apply
+    bg-transparent hover:bg-primary hover:text-black text-primary border-[3px] border-primary w-[100px] 
+    h-[60px] flex justify-center items-center rounded-md cursor-pointer transition-all duration-300 
+    relative font-medium hover:animate-none text-[1.2rem]
+}
+.btn::after {
+    content: '';
+    transform: 
+        perspective(.3em)
+        rotateX(5deg)
+        scale(1.3, .5);
+    filter: blur(10px);
+    @apply
+    absolute left-0 bg-primary bottom-[-50px] h-[40px] w-[100px] opacity-40 pointer-events-none
+    transition-opacity duration-300
+}
+.btn:hover::after {
+    opacity: .7;
 }
 </style>
