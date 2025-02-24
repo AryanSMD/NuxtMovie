c3c0d0c33a57d301fd9d1fac0bb4fddc3788fa9b
AryanSMD <aryansmd79@gmail.com>
2025-02-24 18:30:41 +0330

    updated

--- server/api/img/[...path].ts
+++ server/api/img/[...path].ts
@@ -4,7 +4,7 @@ const ipx = createIPX({
     storage: ipxFSStorage({ dir: "./public" }),
     httpStorage: ipxHttpStorage({ domains: ["image.tmdb.org"] }),
     alias: {
-        '/img': 'https://image.tmdb.org/t/p/original/'
+        '/img': useRuntimeConfig().public.ImageURL,
     }
 })
 
